a8df220d390cd1031877e3b6da57e26d8fcd07b1
zelote123 <fredsonata@gmail.com>
2026-06-18 19:27:39 +0900

    git init

---
+++ src/main/java/com/team6/moduply/Sb10ModuplyTeam6Application.java
@@ -0,0 +1,13 @@
+package com.team6.moduply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb10ModuplyTeam6Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sb10ModuplyTeam6Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/team6/moduply/Sb10ModuplyTeam6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.moduply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb10ModuplyTeam6ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
