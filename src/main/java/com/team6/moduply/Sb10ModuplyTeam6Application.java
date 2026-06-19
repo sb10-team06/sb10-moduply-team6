8dbf1ae3b8ee841c099189399d4668d9c1c0ea7f
박린 <lynnpark2003@ehwa.ac.kr>
2026-06-19 16:29:46 +0900

    refactor: @EnableJpaAuditing 별도의 Config 클래스로 분리

--- src/main/java/com/team6/moduply/Sb10ModuplyTeam6Application.java
+++ src/main/java/com/team6/moduply/Sb10ModuplyTeam6Application.java
@@ -2,10 +2,8 @@ package com.team6.moduply;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class Sb10ModuplyTeam6Application {
 
   public static void main(String[] args) {
