c8bb85acc864806b967561aef86435ba71bff73c
박린 <lynnpark2003@ehwa.ac.kr>
2026-06-23 10:49:37 +0900

    refactor: 코드레빗 리뷰 반영

--- src/main/java/domain/playlist/entity/PlaylistContent.java
+++ src/main/java/domain/playlist/entity/PlaylistContent.java
@@ -3,7 +3,11 @@ package domain.playlist.entity;
 import com.team6.moduply.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -11,19 +15,23 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "playlist_contents")
+@Table(
+    name = "playlist_contents",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"playlist_id", "content_id"})
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PlaylistContent extends BaseEntity {
 
-  @Column(nullable = false)
-  private UUID playlistId;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "playlist_id", nullable = false)
+  private Playlist playlistId;
 
   @Column(nullable = false)
   private UUID contentId;
 
   @Builder
-  public PlaylistContent(UUID playlistId, UUID contentId) {
+  public PlaylistContent(Playlist playlistId, UUID contentId) {
     this.playlistId = playlistId;
     this.contentId = contentId;
   }
