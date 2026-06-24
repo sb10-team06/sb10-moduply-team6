b2d0a5d2f3d38be0201d52af7b4e467922c7c0e8
kwakinsung <kis2690@gmail.com>
2026-06-24 17:34:47 +0900

    feat: BinaryContentService, Repository 셋팅

---
+++ src/main/java/com/team6/moduply/binarycontent/service/BinaryContentService.java
@@ -0,0 +1,9 @@
+package com.team6.moduply.binarycontent.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BinaryContentService {
+}
