29fb43c96c771bbf21abf2c1e500bf60fa3f78e3
zelote123 <fredsonata@gmail.com>
2026-06-24 13:49:37 +0900

    refactor: 리뷰어 피드백 반영
    - 예외처리의 도메인 구분을 위한 code필드 변경

--- src/main/java/com/team6/moduply/user/exception/UserErrorCode.java
+++ src/main/java/com/team6/moduply/user/exception/UserErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-  DUPLICATED_EMAIL_EXCEPTION(HttpStatus.CONFLICT, "DUPLICATED_EMAIL_EXCEPTION", "중복된 이메일입니다."),
-  USER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "USER_NOT_FOUND_EXCEPTION", "사용자를 찾을 수 없습니다.");
+  DUPLICATED_EMAIL_EXCEPTION(HttpStatus.CONFLICT, "user01", "중복된 이메일입니다."),
+  USER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "user02", "사용자를 찾을 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String code;
