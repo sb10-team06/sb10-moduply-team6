3b684f5777fa1f320276f72fe45e87727b4f3cf7
zelote123 <fredsonata@gmail.com>
2026-06-24 11:28:29 +0900

    feat: 사용자 단건 조회 구현
    - Userservice findbyId추가
    - UserController 앤드포인트 추가
    - 커스텀 예외 추가

--- src/main/java/com/team6/moduply/user/service/UserService.java
+++ src/main/java/com/team6/moduply/user/service/UserService.java
@@ -1,6 +1,5 @@
 package com.team6.moduply.user.service;
 
-import com.team6.moduply.common.error.ErrorCode;
 import com.team6.moduply.user.dto.UserCreateRequest;
 import com.team6.moduply.user.dto.UserDto;
 import com.team6.moduply.user.entity.User;
@@ -10,6 +9,7 @@ import com.team6.moduply.user.exception.UserException;
 import com.team6.moduply.user.mapper.UserMapper;
 import com.team6.moduply.user.repository.UserRepository;
 import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -30,19 +30,19 @@ public class UserService {
     log.debug("회원가입 처리 시작");
     if(userRepository.existsByEmail(request.getEmail())){
       log.warn("회원가입 실패: 중복 이메일");
-      // 추후 커스텀 예외로 변경
+
       throw new UserException(UserErrorCode.DUPLICATED_EMAIL_EXCEPTION, Map.of(
           "email", request.getEmail()
       ));
     }
     String encodedPassword = passwordEncoder.encode(request.getPassword());
-    // 추후 비밀번호 인코딩된 비밀번호로 변경
+
     User user = new User(request.getEmail(), encodedPassword, request.getName(), Role.USER);
     try {
       userRepository.save(user);
     } catch (DataIntegrityViolationException e) {
       log.warn("회원가입 실패: 이메일 유니크 제약 위반", e);
-      // 추후 커스텀 예외로 변경
+
       throw new UserException(UserErrorCode.DUPLICATED_EMAIL_EXCEPTION, Map.of(
           "email", request.getEmail()
       ));
@@ -50,6 +50,20 @@ public class UserService {
 
     UserDto response = userMapper.toDto(user);
     log.debug("회원가입 처리 완료. userId={}", response.getId());
+    return response;
+  }
+
+  @Transactional(readOnly = true)
+  public UserDto getUser(UUID userId) {
+    log.debug("사용자 단건 조회 처리 시작. userId={}", userId);
+
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND_EXCEPTION, Map.of(
+            "userId", userId
+        )));
+
+    UserDto response = userMapper.toDto(user);
+    log.debug("사용자 단건 조회 처리 완료. userId={}", response.getId());
     return response;
   }
 }
