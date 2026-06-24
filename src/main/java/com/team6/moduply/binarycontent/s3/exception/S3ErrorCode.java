ec79f81d5cc9e7d99ac2a50a794e4e5d90ed6449
kwakinsung <kis2690@gmail.com>
2026-06-24 10:45:27 +0900

    refactor: 500번대 ERROR INTERNAL_SERVER_ERROR(500)로통일

--- src/main/java/com/team6/moduply/binarycontent/s3/exception/S3ErrorCode.java
+++ src/main/java/com/team6/moduply/binarycontent/s3/exception/S3ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum S3ErrorCode implements ErrorCode {
-    S3_UPLOAD_FAILED(HttpStatus.BAD_GATEWAY, "S301", "S3 파일 업로드에 실패했습니다.");
+    /// 500번대 ERROR는 INTERNAL_SERVER_ERROR로 통일
+    S3_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S301", "S3 파일 업로드에 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
