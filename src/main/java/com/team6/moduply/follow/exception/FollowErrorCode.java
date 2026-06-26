fb74d13a8b272862d19745d5293b7292ac2f59a4
kwakinsung <kis2690@gmail.com>
2026-06-26 14:17:59 +0900

    refactor: 코드래빗 리뷰 반영

--- src/main/java/com/team6/moduply/follow/exception/FollowErrorCode.java
+++ src/main/java/com/team6/moduply/follow/exception/FollowErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FollowErrorCode implements ErrorCode {
   SELF_FOLLOW_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "follow01", "Cannot follow yourself."),
-  FOLLOW_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "follow02", "Follow already exists.");
+  FOLLOW_ALREADY_EXISTS(HttpStatus.CONFLICT, "follow02", "Follow already exists.");
 
   private final HttpStatus httpStatus;
   private final String code;
