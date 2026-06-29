9b77d055c86b1ff7db036b48c975517f9e747009
kwakinsung <kis2690@gmail.com>
2026-06-29 13:11:32 +0900

    feat: 팔로우 취소 API 구현

--- src/main/java/com/team6/moduply/follow/controller/FollowController.java
+++ src/main/java/com/team6/moduply/follow/controller/FollowController.java
@@ -1,18 +1,19 @@
 package com.team6.moduply.follow.controller;
 
-import com.team6.moduply.auth.userdetails.ModuPlyUserDetails;
 import com.team6.moduply.follow.dto.FollowDto;
 import com.team6.moduply.follow.dto.FollowRequest;
 import com.team6.moduply.follow.service.FollowService;
+import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,9 +28,23 @@ public class FollowController implements FollowApi {
   @Override
   public ResponseEntity<FollowDto> createFollow(
       @RequestBody @Valid FollowRequest request,
+      @Parameter(hidden = true)
       @AuthenticationPrincipal(expression = "userDto.id") UUID followerId
   ) {
 
     return ResponseEntity.status(HttpStatus.CREATED).body(followService.createFollow(request, followerId));
+  }
+
+  @DeleteMapping("/{followId}")
+  @Override
+  public ResponseEntity<Void> cancelFollow(
+      @PathVariable UUID followId,
+      @Parameter(hidden = true)
+      @AuthenticationPrincipal(expression = "userDto.id") UUID followerId
+  ) {
+    // followId: 팔로우 관계자체 고유 id
+    // followerId: 팔로우 취소하는 나의 사용자 id
+    followService.cancelFollow(followId, followerId);
+    return ResponseEntity.noContent().build();
   }
 }
