751da0b55c9e33a4401e18ae5e8f9976b9cdbe77
Minbro-Kim <sojangjangi@gmail.com>
2026-06-30 18:40:41 +0900

    refactor: 불필요한 임포트문 제거

--- src/main/java/com/team6/moduply/common/config/AsyncConfig.java
+++ src/main/java/com/team6/moduply/common/config/AsyncConfig.java
@@ -1,22 +1,15 @@
 package com.team6.moduply.common.config;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ThreadPoolExecutor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskDecorator;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableAsync
