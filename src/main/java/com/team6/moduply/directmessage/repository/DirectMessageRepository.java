bf02714777e3c693bdd452f1ba0e39036f964c66
kwakinsung <kis2690@gmail.com>
2026-06-30 16:39:09 +0900

    chore: DirectMessageRepository 최근 메세지 조회메서드 TODO 표시

--- src/main/java/com/team6/moduply/directmessage/repository/DirectMessageRepository.java
+++ src/main/java/com/team6/moduply/directmessage/repository/DirectMessageRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface DirectMessageRepository extends JpaRepository<DirectMessage, UUID> {
 
   /// 특정 대화방에 해당하는 DM중 생성일자를 내림차순으로 정렬했을때 가낭 위에꺼: 가장 최신 DM
+  /// // TODO: 최신 DM을 조회하는 과정에서 CreatedAt이 같을경우를 생각.
   Optional<DirectMessage> findTopByConversationIdOrderByCreatedAtDesc(UUID conversationId);
 
   /// 대화방에서 senderId에 해당하는 사람이 상대방 메시지를 읽지않은게 있는지?
