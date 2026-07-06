027a7e8f1e74ab539fa5aad8e15caa927a5cf294
zelote123 <fredsonata@gmail.com>
2026-07-06 10:17:21 +0900

    test: 테스트 수정

--- src/test/java/com/team6/moduply/auth/oauth2/OAuth2UserInfoFactoryTest.java
+++ src/test/java/com/team6/moduply/auth/oauth2/OAuth2UserInfoFactoryTest.java
@@ -3,6 +3,8 @@ package com.team6.moduply.auth.oauth2;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.team6.moduply.auth.exception.AuthErrorCode;
+import com.team6.moduply.auth.exception.AuthException;
 import com.team6.moduply.auth.oauth2.info.OAuth2UserInfo;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
@@ -24,7 +26,7 @@ class OAuth2UserInfoFactoryTest {
     OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo("google", attributes);
 
     // Then
-    assertThat(userInfo.getProvider()).isEqualTo("google");
+    assertThat(userInfo.getProvider()).isEqualTo("GOOGLE");
     assertThat(userInfo.getProviderId()).isEqualTo("google-provider-id");
     assertThat(userInfo.getEmail()).isEqualTo("tester@example.com");
     assertThat(userInfo.getNickname()).isEqualTo("tester");
@@ -47,7 +49,7 @@ class OAuth2UserInfoFactoryTest {
     OAuth2UserInfo userInfo = OAuth2UserInfoFactory.getOAuth2UserInfo("kakao", attributes);
 
     // Then
-    assertThat(userInfo.getProvider()).isEqualTo("kakao");
+    assertThat(userInfo.getProvider()).isEqualTo("KAKAO");
     assertThat(userInfo.getProviderId()).isEqualTo("12345");
     assertThat(userInfo.getEmail()).isEqualTo("카카오_닉__12345@kakao.com");
     assertThat(userInfo.getNickname()).isEqualTo("카카오 닉!");
@@ -58,7 +60,8 @@ class OAuth2UserInfoFactoryTest {
   void get_oauth2_user_info_fail_when_provider_unsupported() {
     // When & Then
     assertThatThrownBy(() -> OAuth2UserInfoFactory.getOAuth2UserInfo("unknown", Map.of()))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessageContaining("지원하지 않는 소셜 로그인 공급자");
+        .isInstanceOf(AuthException.class)
+        .extracting("errorCode")
+        .isEqualTo(AuthErrorCode.INVALID_PROVIDER_EXCEPTION);
   }
 }
