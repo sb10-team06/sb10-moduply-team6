595f8febe0e867cf1b680d4f0e60c9fb6c5c901b
박린 <lynnpark2003@ehwa.ac.kr>
2026-07-08 13:16:09 +0900

    chore: import문 추가

--- src/test/java/com/team6/moduply/notification/NotificationControllerTest.java
+++ src/test/java/com/team6/moduply/notification/NotificationControllerTest.java
@@ -34,6 +34,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
