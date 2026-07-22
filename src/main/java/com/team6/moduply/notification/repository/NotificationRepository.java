c0fe7ebab9ae539cd876e0c5bb901d71f88b6ca1
Lynn <mac@lynn-MacBook-Pro.local>
2026-07-22 17:40:08 +0900

    refactor: Slice 도입

--- src/main/java/com/team6/moduply/notification/repository/NotificationRepository.java
+++ src/main/java/com/team6/moduply/notification/repository/NotificationRepository.java
@@ -4,9 +4,10 @@ import com.team6.moduply.notification.entity.Notification;
 
 import org.springframework.data.domain.Pageable;
 import java.time.Instant;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,7 +23,7 @@ public interface NotificationRepository extends JpaRepository<Notification, UUID
            OR (n.createdAt = :lastCreatedAt AND n.id > :lastId))
     ORDER BY n.createdAt ASC, n.id ASC
     """)
-  List<Notification> findMissedNotifications(
+  Slice<Notification> findMissedNotifications(
           @Param("receiverId") UUID receiverId,
           @Param("lastCreatedAt") Instant lastCreatedAt,
           @Param("lastId") UUID lastId,
